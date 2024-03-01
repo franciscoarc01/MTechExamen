22205c9de40ba1b9f83b352d0951556681cd77cb
franciscoarc01 <franciscoarc9819@outlook.com>
2024-02-29 19:24:46 -0600

    DataHandler

---
+++ Biblioteca/Escritura.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Escritura
+    {
+        public static void EscrituraFuncion(string path, string[] datos)
+        {
+            StreamWriter sw = new StreamWriter(path);
+            for (uint datoInd = 0; datoInd < datos.Length; datoInd++)
+            {
+                sw.WriteLine(datos[datoInd]);
+            }
+            sw.Close();
+        }
+    }
+}
---
+++ Biblioteca/Lectura.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Lectura
+    {
+        public static string[] Lista(string path)
+        {
+            return File.ReadAllLines(path);
+        }
+    }
+}
